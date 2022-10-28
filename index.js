79c169077e4d9b2e01b7207fb45b1d6939b7d9bf
Dan Barr <dan.barr@outlook.com>
2022-10-28 17:20:28 -0400

    wildly inconsistent spacing, method names too long

--- index.js
+++ index.js
@@ -29,28 +29,30 @@ function dostuff() {
                                 new FrequencyValues(24.99, 4.1, 1.5),
                                 new FrequencyValues(29.7, 2.18, 4.5)];
 
-    all_frequency_values.forEach( f => {
-        let yarg = calculate_uncontrolled_safe_distance(f, cable_values, xmtr_power, feedline_length, duty_cycle, per_30 );
+    all_frequency_values.forEach(f => {
+        let yarg = calc_uncontrolled_safe_distance(f, cable_values, xmtr_power, feedline_length, duty_cycle, per_30 );
         console.log(yarg);
     })
 }
 
-function calculate_uncontrolled_safe_distance( freq_values, cable_values, transmitter_power, feedline_length, duty_cycle,
+function calc_uncontrolled_safe_distance(freq_values, cable_values, transmitter_power, feedline_length, duty_cycle,
     uncontrolled_percentage_30_minutes) {
 
-    let gamma = calculate_reflection_coefficient( freq_values )
+    let gamma = calc_reflection_coefficient(freq_values)
 
-    let feedline_loss_per_100ft_at_frequency = calculate_feedline_loss_per_100ft_at_frequency( freq_values, cable_values )
+    let feedline_loss_per_100ft_at_frequency = calc_feedline_loss_per_100ft_at_frequency(freq_values, cable_values)
     
-    let feedline_loss_for_matched_load_at_frequency = calculate_feedline_loss_for_matched_load_at_frequency( feedline_length, feedline_loss_per_100ft_at_frequency )
+    let feedline_loss_for_matched_load_at_frequency = 
+        calc_feedline_loss_for_matched_load_at_frequency(feedline_length, feedline_loss_per_100ft_at_frequency)
     
-    let feedline_loss_for_matched_load_at_frequency_percentage = calculate_feedline_loss_for_matched_load_at_frequency_percentage( feedline_loss_for_matched_load_at_frequency)
+    let feedline_loss_for_matched_load_at_frequency_percentage = 
+        calc_feedline_loss_for_matched_load_at_frequency_percentage(feedline_loss_for_matched_load_at_frequency)
     
-    let gamma_squared = Math.abs( gamma )**2
+    let gamma_squared = Math.abs(gamma)**2
     
-    let feedline_loss_for_swr = calculate_feedline_loss_for_swr( feedline_loss_for_matched_load_at_frequency_percentage, gamma_squared )
+    let feedline_loss_for_swr = calc_feedline_loss_for_swr(feedline_loss_for_matched_load_at_frequency_percentage, gamma_squared)
     
-    let feedline_loss_for_swr_percentage = calculate_feedline_loss_for_swr_percentage( feedline_loss_for_swr )
+    let feedline_loss_for_swr_percentage = calc_feedline_loss_for_swr_percentage(feedline_loss_for_swr)
     
     let power_loss_at_swr = feedline_loss_for_swr_percentage * transmitter_power
     
@@ -58,37 +60,37 @@ function calculate_uncontrolled_safe_distance( freq_values, cable_values, transm
     
     let uncontrolled_average_pep = peak_envelope_power_at_antenna * duty_cycle * uncontrolled_percentage_30_minutes
     
-    let mpe_s = 180/( freq_values.freq**2 )
+    let mpe_s = 180/(freq_values.freq**2)
     
-    let gain_decimal = 10**( freq_values.gaindbi/10 )
+    let gain_decimal = 10**(freq_values.gaindbi/10)
     
     //TODO:  Include effects of ground reflections
     
-    return Math.sqrt( ( 0.219 * uncontrolled_average_pep * gain_decimal )/mpe_s )
+    return Math.sqrt((0.219 * uncontrolled_average_pep * gain_decimal)/mpe_s)
 }
 
-function calculate_reflection_coefficient(freq_values) {
-    return Math.abs( freq_values.swr - 1)/(freq_values.swr + 1);
+function calc_reflection_coefficient(freq_values) {
+    return Math.abs(freq_values.swr - 1)/(freq_values.swr + 1);
 }
 
-function calculate_feedline_loss_for_matched_load_at_frequency( feedline_length, feedline_loss_per_100ft_at_frequency) {
+function calc_feedline_loss_for_matched_load_at_frequency(feedline_length, feedline_loss_per_100ft_at_frequency) {
     return (feedline_length/100) * feedline_loss_per_100ft_at_frequency;
 }
 
 
-function calculate_feedline_loss_for_matched_load_at_frequency_percentage(feedline_loss_for_matched_load) {
-    return (10**(-feedline_loss_for_matched_load/10));
+function calc_feedline_loss_for_matched_load_at_frequency_percentage(feedline_loss_for_matched_load) {
+    return 10**(-feedline_loss_for_matched_load/10);
 }
 
-function calculate_feedline_loss_per_100ft_at_frequency(freq_values, cable_values) {
+function calc_feedline_loss_per_100ft_at_frequency(freq_values, cable_values) {
     return cable_values.k1 * Math.sqrt(freq_values.freq + cable_values.k2 * freq_values.freq);
 }
 
-function calculate_feedline_loss_for_swr(feedline_loss_for_matched_load_percentage, gamma_squared) {
+function calc_feedline_loss_for_swr(feedline_loss_for_matched_load_percentage, gamma_squared) {
     return -10 * Math.log10(feedline_loss_for_matched_load_percentage * ((1 - gamma_squared)/(1 - feedline_loss_for_matched_load_percentage**2 * gamma_squared)));
 }
 
-function calculate_feedline_loss_for_swr_percentage( feedline_loss_for_swr ) {
+function calc_feedline_loss_for_swr_percentage( feedline_loss_for_swr ) {
     return (100 - 100/( 10**(feedline_loss_for_swr/10)))/100
 }
 
